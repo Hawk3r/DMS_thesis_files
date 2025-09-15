98ceaf5c333891aee1ec7af55b033d6994b2eef6
henson lee <hensonlee1022@gmail.com>
2025-09-15 18:25:40 +0800

    cleaning up repository

--- Android test v5/test.py
+++
@@ -1,9 +0,0 @@
-# main.py
-from client import GeminiChatbot
-
-bot = GeminiChatbot("")
-
-print(bot.send_message("Hello!"))
-print(bot.send_message("What's the capital of France?"))
-print(bot.send_message("And what language do they speak there?"))
-
--- app_codes_v6/tts.py
+++
@@ -1,6 +0,0 @@
-import pyttsx3
-engine = pyttsx3.init()
-engine.say("What")
-print("hi")
-engine.runAndWait()
-print("hello")
